710db34b9aa672d72dd99ebe5660d01b54bc4b4e
shashank7200 <shashank7200@gmail.com>
2017-09-11 10:10:59 +0530

    added firebase support to update counter count

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import * as firebase from "firebase";
 
-class App extends Component {  
+//  Imports configuration for firebase app
+import config from "./firebase.config.js";
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      count: ""
+    };
+  }
+
+  componentWillMount() {
+    firebase.initializeApp(config);
+    const _this = this;
+    const counter = firebase.database().ref("counter");
+    counter.on("value", snapshot => {
+      _this.setState({
+        count: snapshot.val().count
+      });
+    });
+  }
 
   // Function to call the increment dispatching event
-  incrementCount(){
-    const { incrementCount} = this.props;
-    incrementCount();
+  incrementCount() {
+    const counter = firebase.database().ref("counter");
+    counter.set({
+      count: this.state.count + 1
+    });
   }
 
   // Function to call the decrement dispatching event
-  decrementCount(){
-    const { decrementCount } = this.props;
-    decrementCount();
+  decrementCount() {
+    const counter = firebase.database().ref("counter");
+    const { count } = this.state
+    count!==0?counter.set({
+      count: count - 1
+    }):undefined;
+  }
+
+  reset(){
+    const counter = firebase.database().ref("counter");
+    counter.set({
+      count: 0
+    });
   }
 
   render() {
-    const { state:{ count } } = this.props;
+    const { count } = this.state;
 
     return (
       <div className="App">
@@ -27,9 +61,16 @@ class App extends Component {
         </div>
         <div className="app-dashboard">
           <h2>Count Your Clicks</h2>
-          <p className="count" >{ count }</p>
-          <button type="primary" onClick={ this.incrementCount.bind(this) } >Increment</button>
-          <button type="primary" onClick={ this.decrementCount.bind(this) } >Decrement</button>
+          <p className="count">{count}</p>
+          <button type="primary" onClick={this.incrementCount.bind(this)}>
+            Increment
+          </button>
+          <button type="primary" onClick={this.decrementCount.bind(this)}>
+            Decrement
+          </button>
+          <button type="primary" onClick={this.reset.bind(this)}>
+          Reset
+        </button>
         </div>
       </div>
     );
