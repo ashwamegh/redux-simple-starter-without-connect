cd4572d9941e9848f127cffe4e52973c6d6809cc
shashank7200 <shashank7200@gmail.com>
2017-09-11 10:09:40 +0530

    added firebase config

---
+++ src/firebase.config.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: process.env.REACT_API_KEY,
+  authDomain: "fir-counter-b9ec8.firebaseapp.com",
+  databaseURL: "https://fir-counter-b9ec8.firebaseio.com",
+  projectId: "fir-counter-b9ec8",
+  storageBucket: "",
+  messagingSenderId: process.env.REACT_SENDER_ID
+};
