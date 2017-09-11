f1c6aacc655800855638da7b95ca1b8e738b32d0
shashank7200 <shashank7200@gmail.com>
2017-09-11 10:11:26 +0530

    removed redux and its dependency from app

--- src/index.js
+++ src/index.js
@@ -3,56 +3,7 @@ import { render } from "react-dom";
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
-import { createStore } from "redux";
 
-// Variables containing Action types
-const INC = "INC";
-const DEC = "DEC";
-
-// Our reducer (root reducer for now) for the initializing store
-const counterReducer = (state = { count: 0 }, action) => {
-  switch (action.type) {
-    case INC:
-      return Object.assign({}, state, {
-        count: state.count + action.payload.value
-      });
-    case DEC:
-      return state.count !== 0
-        ? Object.assign({}, state, {
-            count: state.count - action.payload.value
-          })
-        : state;
-    default:
-      return state;
-  }
-};
-
-// our redux store holder
-const store = createStore(counterReducer);
-
-// Action creators for the counter actions
-const actionsCreators = {
-  incrementActionCreator: () => ({ type: INC, payload: { value: 1 } }),
-  decrementActionCreator: () => ({ type: DEC, payload: { value: 1 } })
-};
-
-// Store Listener
-const listenStore = () =>
-  render(
-    <App
-      state={store.getState()}
-      incrementCount={() =>
-        store.dispatch(actionsCreators.incrementActionCreator())}
-      decrementCount={() =>
-        store.dispatch(actionsCreators.decrementActionCreator())}
-    />,
-    document.getElementById("root")
-  );
-
-// Subscribing listenStore function to listen and subscribe the state changes
-store.subscribe(listenStore);
-
-// Callin the function to render the updated state
-listenStore();
+render(<App />, document.getElementById("root"));
 
 registerServiceWorker();
